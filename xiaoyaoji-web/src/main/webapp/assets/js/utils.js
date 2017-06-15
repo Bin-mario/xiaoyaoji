e2a824379099a3724565db902b94f11aa3231025
org.zhoujingjie@gmail.com <org.zhoujingjie@gmail.com>
2017-06-15 11:27:08 +0800

    修改slice为深拷贝

--- xiaoyaoji-web/src/main/webapp/assets/js/utils.js
+++ xiaoyaoji-web/src/main/webapp/assets/js/utils.js
@@ -296,7 +296,8 @@
         };
 
         Array.prototype.mergeArray = function(source){
-            var target = this.slice();
+            //var target = this.slice();
+            var target = $.extend(true,{},this);
             if(source && source.length>0){
                 var targetKey={};
                 target.forEach(function(item){
